bbd8c4202f1578c94f80a2939c70c093f9bb3a95
Ruslan3571 <ruslan12032@gmail.com>
2022-12-22 15:56:37 +0200

    +

---
+++ index.js
@@ -0,0 +1,23 @@
+// Ввод и вывод данных, оператор присваивания
+
+// Begin1◦
+// . Дана сторона квадрата a. Найти его периметр P = 4·a.
+
+// let sideA = 0;
+
+// function perimetr(sideA) {
+//   return sideA * 4;
+// }
+
+// const perimetr = (sideA) => sideA * 4;
+
+// console.log(perimetr(5));
+
+// <-----------------------------------------NEW TASK----------------------------------------->
+
+// Begin2◦
+// . Дана сторона квадрата a. Найти его площадь S = a2
+
+// let sideA = 0;
+// const square = (sideA) => sideA ** 2;
+// console.log("square: ", square(2));
